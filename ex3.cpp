914c58681ccbe7463fd0917a99dcd9c7cfec35de
Nigel Saurino <nigelsaurino@gmail.com>
2024-07-17 17:31:45 -0400

    Finished week 2 assignment

--- ex3.cpp
+++ ex3.cpp
@@ -2,6 +2,7 @@
 #include "ex1.cpp"
 #include <utility>
 #include <tuple>
+#include <variant>
 
 /*
 - Use `std::pair` and `std::tuple` to store and manipulate related data.
@@ -9,21 +10,36 @@
 - Use `std::variant` to hold one of several possible types.
 */
 
+// optional can be used as the return type of a factory that may fail
+std::optional<std::string> create(bool b)
+{
+    if (b)
+        return "Godzilla";
+    return {};
+}
+
 int Ex3(){
 
     // Pair and Tuple Examples
-    std::pair<int, int> coords = std::make_pair(0,0);
+    std::pair<int, int> coords{0,0};
     coords.first = 1;
     coords.second = 2;
 
     std::cout << coords.first << ", " << coords.second << std::endl;
 
-    std::tuple<int, char, int> tups(std::make_tuple(0, 'c', 1));
+    std::tuple<int, char, int> tups{0, 'c', 1};
     std::get<0>(tups) = 7;
     std::cout << std::get<0>(tups) << ", " << std::get<1>(tups) << ", " << std::get<2>(tups) << std::endl;
 
-    // std::optional examples
-    
+    // std::optional example
+    std::cout << create(true).value_or("empty") << std::endl;
+
+    // std::variant example
+    std::variant<int, double> variant_ex;
+    variant_ex = 42;
+    std::cout << std::get<0>(variant_ex) << std::endl;
+    variant_ex = 42.2;
+    std::cout << std::get<1>(variant_ex) << std::endl;
 
     return 0;
 }
